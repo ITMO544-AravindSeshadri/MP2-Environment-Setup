01e714e516acc130f35d3e41bd9b1c82ca28213f
Aravind Seshadri <aravsesh1@gmail.com>
2015-11-14 14:22:22 -0600

    Path change in install-env.sh

--- install-env.sh
+++ install-env.sh
@@ -3,9 +3,9 @@ sudo apt-get update && sudo apt-get install -y apache2 git php5 php5-mysql php5-
 touch /tmp/hello.txt
 echo "Hello" > /tmp/hello.txt
 cd /var/www/html
-sudo git clone https://github.com/ITMO544-AravindSeshadri/MP1-Application-Setup.git
+sudo git clone https://github.com/ITMO544-AravindSeshadri/MP2-Application-Setup.git
 sudo mv -v MP1-Application-Setup/* /var/www/html
-sudo rm -rf /var/www/html/MP1-Application-Setup
+sudo rm -rf /var/www/html/MP2-Application-Setup
 cd /home/controller
 sudo curl -sS https://getcomposer.org/installer | php
 sudo php composer.phar require aws/aws-sdk-php
