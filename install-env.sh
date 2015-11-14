9ef6f3e67fb6a1ec98f54ba1a131a8bb3f243ff0
Aravind Seshadri <aravsesh1@gmail.com>
2015-11-14 14:11:04 -0600

    Adding install-env.sh

---
+++ install-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get update && sudo apt-get install -y apache2 git php5 php5-mysql php5-curl mysql-client curl
+touch /tmp/hello.txt
+echo "Hello" > /tmp/hello.txt
+cd /var/www/html
+sudo git clone https://github.com/ITMO544-AravindSeshadri/MP1-Application-Setup.git
+sudo mv -v MP1-Application-Setup/* /var/www/html
+sudo rm -rf /var/www/html/MP1-Application-Setup
+cd /home/controller
+sudo curl -sS https://getcomposer.org/installer | php
+sudo php composer.phar require aws/aws-sdk-php
+PW=letmein
+export PW
